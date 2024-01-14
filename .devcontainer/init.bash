ef4d05ef72fc63474d11ea228e7696842db17166
zhenhua32 <zhenhua32@gmail.com>
2024-01-14 14:27:14 +0800

    保存配置文件

---
+++ .devcontainer/init.bash
@@ -0,0 +1,6 @@
+# 配置 git 的换行符
+git config --global core.autocrlf true
+# 使用清华源
+python -m pip install --upgrade pip
+pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+
--- cli_demo.py
+++ cli_demo.py
@@ -19,6 +19,7 @@ clear_command = "cls" if os_name == "Windows" else "clear"
 
 
 class BlueLMStreamer(TextStreamer):
+    """从 TextStreamer 继承，重写 put 和 end 方法"""
     def __init__(self, tokenizer: "AutoTokenizer"):
         self.tokenizer = tokenizer
         self.tokenIds = []
@@ -32,11 +33,14 @@ class BlueLMStreamer(TextStreamer):
             self.first = False
             return
         self.tokenIds.append(value.item())
+        # 解码并跳过特殊字符
         text = tokenizer.decode(self.tokenIds, skip_special_tokens=True)
+        # 能被 4 整除是什么道理?
         if len(self.tokenIds) % 4 == 0 and text and text[-1] != "�":
             self.build_display(self.history, self.prompt, text)
 
     def end(self):
+        """结束后重置状态"""
         self.first = True
         text = tokenizer.decode(self.tokenIds, skip_special_tokens=True)
         self.response = text
@@ -44,6 +48,7 @@ class BlueLMStreamer(TextStreamer):
         self.tokenIds = []
 
     def build_display(self, history, prompt, cur_text):
+        """清空并显示输出"""
         info_str = "欢迎使用 BlueLM-7B 模型，输入内容即可进行对话，clear 清空对话历史，quit 终止程序"
         for query, response in history:
             info_str += f"\n\nUser：{query}"
@@ -55,9 +60,11 @@ class BlueLMStreamer(TextStreamer):
 
 
 def build_prompt(history, prompt):
+    """构建输入格式"""
     res = ""
     for query, response in history:
         res += f"[|Human|]:{query}[|AI|]:{response}</s>"
+    # 每一句话需要是这种格式
     res += f"[|Human|]:{prompt}[|AI|]:"
     return res
 
@@ -72,6 +79,7 @@ def main():
         if query.strip() == "quit":
             break
         if query.strip() == "clear":
+            # 重新开始
             history = []
             os.system(clear_command)
             print("欢迎使用 BlueLM-7B 模型，输入内容即可进行对话，clear 清空对话历史，quit 终止程序")
